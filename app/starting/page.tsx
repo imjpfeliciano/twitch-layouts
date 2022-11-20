ebc2e7c82187dbffc272163e1192380e693d572d
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2022-11-19 22:23:34 -0600

    fix: update font-size

--- app/starting/page.tsx
+++ app/starting/page.tsx
@@ -1,7 +1,7 @@
 const StartingPage = () => {
   return (
     <div className="aspect-video bg-green-500 flex justify-center align-center items-center">
-      <h1 className="text-white text-6xl font-bold bg-blue-200">
+      <h1 className="text-white text-9xl font-bold bg-blue-200">
         Starting Page
       </h1>
     </div>
